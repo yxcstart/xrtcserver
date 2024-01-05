bead92b9625b98b1618ed78c7f3ed92faa678dda
geekyang <geekyang@tencent.com>
2024-01-05 10:19:56 +0800

    compute ice agent state

--- src/ice/ice_agent.h
+++ src/ice/ice_agent.h
@@ -23,20 +23,27 @@ public:
                                const IceParameters& ice_params);
 
     void gathering_candidate();
+    IceTransportState ice_state() { return _ice_state; }
 
     sigslot::signal4<IceAgent*, const std::string&, IceCandidateComponent, const std::vector<Candidate>&>
         signal_candidate_allocate_done;
+    sigslot::signal2<IceAgent*, IceTransportState> signal_ice_state;
 
 private:
     std::vector<IceTransportChannel*>::iterator _get_channel(const std::string& transport_name,
                                                              IceCandidateComponent component);
 
     void on_candidate_allocate_done(IceTransportChannel* channel, const std::vector<Candidate>&);
+    void _on_ice_receiving_state(IceTransportChannel* channel);
+    void _on_ice_writable_state(IceTransportChannel* channel);
+    void _on_ice_state_changed(IceTransportChannel* channel);
+    void _update_state();
 
 private:
     EventLoop* _el;
     std::vector<IceTransportChannel*> _channels;
     PortAllocator* _allocator;
+    IceTransportState _ice_state = IceTransportState::k_new;
 };
 
 }  // namespace xrtc
